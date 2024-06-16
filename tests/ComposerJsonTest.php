5dc40b6f6cc8ef33d949b590eef38f04affe51c3
Iman <imanghafoori1@users.noreply.github.com>
2024-06-16 09:25:44 +0000

    Apply fixes from StyleCI
    [ci skip] [skip ci]

--- tests/ComposerJsonTest.php
+++ tests/ComposerJsonTest.php
@@ -225,6 +225,7 @@ class ComposerJsonTest extends TestCase
 
         $this->assertEquals($expected, $actual);
     }
+
     public function test_get_classmap()
     {
         $reader = ComposerJson::make(__DIR__.'/Stubs/a3');
